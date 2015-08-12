d70e80d7f4bdaef77c7c73539544887df67051fd
Glen Langer <contao.modul@glen-langer.de>
2015-08-12 21:17:33 +0200

    Update language files (ja)

---
+++ languages/ja/modules.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/contao-dlstats_statistic_export/language/ja/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-08-12T21:05:45+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['MOD']['dlstats_statistic_export']['0'] = '統計エクスポートのdlstatsアドオン';
+$GLOBALS['TL_LANG']['MOD']['dlstats_statistic_export']['1'] = '統計のエクスポートをできるようにします。';
+
--- languages/ja/tl_dlstats_stat_export.php
+++ languages/ja/tl_dlstats_stat_export.php
@@ -8,15 +8,25 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2014-10-26T15:14:03+01:00
+ * last-updated: 2015-08-12T21:05:45+02:00
  */
 
-
-$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['all_months']        = 'すべての月';
-$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['all_years']         = 'すべての年';
-$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['export']            = 'エクスポート';
-$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['format']            = '書式';
-$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['month']             = '月';
-$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['statistics_export'] = '統計をエクスポート';
-$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['year']              = '年';
+$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['all_months']                = 'すべての月';
+$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['all_years']                 = 'すべての年';
+$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['csv']                       = 'CSV .csv';
+$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['excel']                     = 'Excel .xlsx';
+$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['export']                    = 'エクスポート';
+$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['export_field_browser_lang'] = 'ブラウザーの言語';
+$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['export_field_date']         = '日付';
+$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['export_field_domain']       = 'ドメイン';
+$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['export_field_filename']     = 'ファイル名';
+$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['export_field_ip']           = 'IP';
+$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['export_field_page_alias']   = 'ページのエイリアス';
+$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['export_field_page_host']    = 'ページのホスト';
+$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['export_field_username']     = 'ユーザー名';
+$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['export_title']              = '統計をダウンロード';
+$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['format']                    = '書式';
+$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['month']                     = '月';
+$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['statistics_export']         = '統計をエクスポート';
+$GLOBALS['TL_LANG']['tl_dlstats_stat_export']['year']                      = '年';
 
